594d0e2b4dc0ba74f50cbee362ccd261e4641884
marumasa25 <118707717+marumasa25@users.noreply.github.com>
2022-11-21 13:25:23 +0900

    Add files via upload

---
+++ millionaire/WebContent/jquery01.js
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+ $(function(){
+	$("#hand1").click(function(){
+		$("#hand1").dialog({
+			modal:true,
+			title:"確認",
+			buttons:{
+				"ok":function(){
+					
+				},
+			}
+		})
+	}
+);
